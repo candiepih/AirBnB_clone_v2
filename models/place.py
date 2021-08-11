e2597cf40f8e5f991f5deb4b3224f76f7fb00270
candiepih <mutheeal.am@gmail.com>
2021-08-11 13:09:05 +0000

    fixed ommited primary keys

--- models/place.py
+++ models/place.py
@@ -7,11 +7,11 @@ from sqlalchemy.orm import relationship
 
 metadata = Base.metadata
 place_amenity = Table("place_amenity", metadata, 
-                         Column("place_id", String(60),
-			        ForeignKey("places.id"), nullable=False),
-                         Column("amenity_id", String(60),
-			        ForeignKey("amenities.id"), nullable=False)
-                     )
+                      Column("place_id", String(60), ForeignKey("places.id"),
+                             primary_key=True, nullable=False),
+                      Column("amenity_id", String(60),
+                             ForeignKey("amenities.id"),
+			     primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
