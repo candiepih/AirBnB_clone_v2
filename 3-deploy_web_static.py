70971baa23b8b611d7efed63deec99ad2df2f759
candiepih <mutheeal.am@gmail.com>
2021-08-25 15:55:44 +0000

    added task 3. Full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,12 +1,27 @@
 #!/usr/bin/python3
-""" Fabric script that generates deploys an archive to the web
+""" Fabric script that generates a .tgz archive from the contents
+    of the web_static folder and deploys an archive to your web
     servers
 """
-from fabric.api import run, put, env
+from fabric.api import local, run, put, env
 from datetime import datetime
-from os.path import isfile, basename
+from os.path import isdir, isfile, basename
 
 env.hosts = ['34.138.28.3', '34.138.208.167']
+
+
+def do_pack():
+    """ Creates a .tgz archive from web_static folder"""
+    string_date = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive = "versions/web_static_{}.tgz".format(string_date)
+
+    try:
+        if not isdir('versions'):
+            local("mkdir versions")
+        local("tar -cvzf {} web_static".format(archive))
+        return archive
+    except:
+        return None
 
 
 def do_deploy(archive_path):
@@ -14,6 +29,7 @@ def do_deploy(archive_path):
     if not isfile(archive_path):
         return False
     filename = basename(archive_path)
+
     try:
         no_ext = filename.split(".")[0]
         put(archive_path, "/tmp/")
@@ -28,3 +44,12 @@ def do_deploy(archive_path):
         return True
     except:
         return False
+
+
+def deploy():
+    """creates and distributes an archive to your web servers"""
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
+    has_deployed = do_deploy(archive_path)
+    return has_deployed
