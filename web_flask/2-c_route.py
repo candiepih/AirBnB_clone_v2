4bc1c8d6306841d437aa6a9ab271ffa3f81d306e
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-09-11 11:30:37 +0300

    Update 2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,3 +1,11 @@
+#!/usr/bin/python3
+"""
+    script that starts a Flask web application:
+    /: display “Hello HBNB!”
+    /hbnb: display “HBNB”
+    /c/<text>: display “C ” followed by the value of the text
+    variable (replace underscore _ symbols with a space)
+"""
 from flask import Flask
 
 app = Flask(__name__)
